54e25084840c812535a981f879b81f8b75a63538
itoulou <touloumbadjiani@gmail.com>
2023-06-06 14:50:33 +0100

    fix tests

--- tests/end_to_end_tests/journey_1/personal_details/section.py
+++ tests/end_to_end_tests/journey_1/personal_details/section.py
@@ -276,6 +276,12 @@ async def run_checks_on_section(page: Page, asserts: AssertHelpers, helpers: Pag
     await helpers.check_radio(field='overseasCheck', value='True')
     await helpers.click_button('Continue')
     await asserts.url('/overseas-approved-check')
+    await helpers.check_radio(field='overseasApprovedCheck', value='True')
+    await helpers.click_button('Continue')
+    await asserts.url('/declaration')
+    await helpers.check_checkbox(field='consent')
+    await helpers.click_button('Continue')
+    await asserts.url('/task-list')
     await helpers.go_to_page(TEST_URL + '/personal-details/transition-date')
     await asserts.url('/personal-details/transition-date')
 
