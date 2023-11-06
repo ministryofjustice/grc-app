48cb1a22de87a50719fbcd1326f8b0e905603bdc
Avi Singh <124276939+avi-singh-1912@users.noreply.github.com>
2023-11-06 09:51:00 +0000

    RST 5949 - automated endpoint test (#93)
    Test to check JSON health response
    ---------
    Co-authored-by: itoulou <35838791+itoulou@users.noreply.github.com>

---
+++ tests/test_ping_json.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def test_json_health_check():
+    # Access health pages, currently does not work with admin app
+    public_response = requests.get("http://localhost:8080/health")
+
+    # Converting into accessible data
+    public_data = public_response.json()
+
+    # Public app assertions
+    assert public_response.status_code == 200
+    assert 'status' in public_data and public_data['status'] == 'success', 'Public app JSON status unsuccessful'
+    assert public_data['results'][0]['passed'] is True, 'Public app JSON results has not passed'
+    print('Public app JSON response is healthy')
+
+
