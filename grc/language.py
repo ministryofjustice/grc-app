6822e5051e80d1e9b4a0814a7378d2a5c5085fb5
itoulou <touloumbadjiani@gmail.com>
2024-01-26 17:26:06 +0000

    use local redirect instead of redirect directly

--- grc/language.py
+++ grc/language.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, session, redirect
+from .utils.redirect import local_redirect
 
 set_language = Blueprint('set_language', __name__)
 
@@ -6,4 +7,4 @@ set_language = Blueprint('set_language', __name__)
 @set_language.route('/set_language/<lang_code>')
 def index(lang_code):
     session['lang_code'] = lang_code
-    return redirect(request.referrer)
+    return local_redirect(request.referrer)
