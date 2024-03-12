f8ef5982bdcb221b623f13a1ad6a161dd2830aed
itoulou <touloumbadjiani@gmail.com>
2024-03-12 17:04:13 +0000

    rename doc checker state var for better practice

--- grc/document_checker/__init__.py
+++ grc/document_checker/__init__.py
@@ -18,16 +18,16 @@ def index():
 @documentChecker.route('/check-documents/changed-name-to-reflect-gender', methods=['GET', 'POST'])
 def previousNamesCheck():
     form = PreviousNamesCheck()
-    doc_checker_state = DocCheckerDataStore.load_doc_checker_state()
+    doc_checker_state_ = DocCheckerDataStore.load_doc_checker_state()
 
     if form.validate_on_submit():
-        doc_checker_state.changed_name_to_reflect_gender = strtobool(form.changed_name_to_reflect_gender.data)
-        DocCheckerDataStore.save_doc_checker_state(doc_checker_state)
+        doc_checker_state_.changed_name_to_reflect_gender = strtobool(form.changed_name_to_reflect_gender.data)
+        DocCheckerDataStore.save_doc_checker_state(doc_checker_state_)
 
         return local_redirect(url_for('documentChecker.currentlyInAPartnership'))
 
     if request.method == 'GET':
-        form.changed_name_to_reflect_gender.data = doc_checker_state.changed_name_to_reflect_gender
+        form.changed_name_to_reflect_gender.data = doc_checker_state_.changed_name_to_reflect_gender
 
     return render_template(
         'document-checker/previous-names-check.html',
@@ -41,7 +41,7 @@ def currentlyInAPartnership():
     doc_checker_state_ = DocCheckerDataStore.load_doc_checker_state()
 
     if form.validate_on_submit():
-        doc_checker_state.currently_in_a_partnership = CurrentlyInAPartnershipEnum(form.currently_in_a_partnership.data)
+        doc_checker_state_.currently_in_a_partnership = CurrentlyInAPartnershipEnum(form.currently_in_a_partnership.data)
         DocCheckerDataStore.save_doc_checker_state(doc_checker_state_)
 
         if doc_checker_state_.is_currently_in_partnership:
@@ -68,21 +68,21 @@ def currentlyInAPartnership():
 @documentChecker.route('/check-documents/plan-to-remain-in-a-partnership', methods=['GET', 'POST'])
 def planToRemainInAPartnership():
     form = PlanToRemainInAPartnershipForm()
-    doc_checker_state = DocCheckerDataStore.load_doc_checker_state()
+    doc_checker_state_ = DocCheckerDataStore.load_doc_checker_state()
 
     if form.validate_on_submit():
-        doc_checker_state.plan_to_remain_in_a_partnership = strtobool(form.plan_to_remain_in_a_partnership.data)
-        DocCheckerDataStore.save_doc_checker_state(doc_checker_state)
+        doc_checker_state_.plan_to_remain_in_a_partnership = strtobool(form.plan_to_remain_in_a_partnership.data)
+        DocCheckerDataStore.save_doc_checker_state(doc_checker_state_)
 
         return local_redirect(url_for('documentChecker.genderRecognitionOutsideUK'))
 
     if request.method == 'GET':
-        form.plan_to_remain_in_a_partnership.data = doc_checker_state.plan_to_remain_in_a_partnership
+        form.plan_to_remain_in_a_partnership.data = doc_checker_state_.plan_to_remain_in_a_partnership
 
     return render_template(
         'document-checker/plan-to-remain-in-a-partnership.html',
         form=form,
-        doc_checker_state=doc_checker_state
+        doc_checker_state=doc_checker_state_
     )
 
 
@@ -129,34 +129,34 @@ def previousPartnershipEnded():
 @documentChecker.route('/check-documents/gender-recognition-outside-uk', methods=['GET', 'POST'])
 def genderRecognitionOutsideUK():
     form = GenderRecognitionOutsideUKForm()
-    doc_checker_state = DocCheckerDataStore.load_doc_checker_state()
+    doc_checker_state_ = DocCheckerDataStore.load_doc_checker_state()
 
     if form.validate_on_submit():
-        doc_checker_state.gender_recognition_outside_uk = strtobool(form.gender_recognition_outside_uk.data)
-        DocCheckerDataStore.save_doc_checker_state(doc_checker_state)
+        doc_checker_state_.gender_recognition_outside_uk = strtobool(form.gender_recognition_outside_uk.data)
+        DocCheckerDataStore.save_doc_checker_state(doc_checker_state_)
 
         return local_redirect(url_for('documentChecker.your_documents'))
 
     if request.method == 'GET':
-        form.gender_recognition_outside_uk.data = doc_checker_state.gender_recognition_outside_uk
+        form.gender_recognition_outside_uk.data = doc_checker_state_.gender_recognition_outside_uk
 
     return render_template(
         'document-checker/gender-recognition-outside-uk.html',
         form=form,
-        doc_checker_state=doc_checker_state
+        doc_checker_state=doc_checker_state_
     )
 
 
 @documentChecker.route('/check-documents/your-documents', methods=['GET', 'POST'])
 def your_documents():
-    doc_checker_state = DocCheckerDataStore.load_doc_checker_state()
+    doc_checker_state_ = DocCheckerDataStore.load_doc_checker_state()
 
     if not hasUserAnswersAllTheQuestions():
         return local_redirect(getUrlForNextUnansweredQuestion())
 
     return render_template(
         'document-checker/your-documents.html',
-        doc_checker_state=doc_checker_state
+        doc_checker_state=doc_checker_state_
     )
 
 
@@ -165,7 +165,7 @@ def askForEmailAddress():
     if not hasUserAnswersAllTheQuestions():
         return local_redirect(getUrlForNextUnansweredQuestion())
 
-    doc_checker_state = DocCheckerDataStore.load_doc_checker_state()
+    doc_checker_state_ = DocCheckerDataStore.load_doc_checker_state()
     form = EmailForm()
 
     if form.validate_on_submit():
@@ -174,37 +174,37 @@ def askForEmailAddress():
                 email_address=
                     form.email_address.data,
                 need_to_send_name_change_documents=
-                    doc_checker_state.need_to_send_name_change_documents,
+                    doc_checker_state_.need_to_send_name_change_documents,
                 need_to_send_medical_reports=
-                    doc_checker_state.need_to_send_medical_reports,
+                    doc_checker_state_.need_to_send_medical_reports,
                 need_to_send_evidence_of_living_in_gender=
-                    doc_checker_state.need_to_send_evidence_of_living_in_gender,
+                    doc_checker_state_.need_to_send_evidence_of_living_in_gender,
                 need_to_send_statutory_declaration_for_single_applicant=
-                    doc_checker_state.need_to_send_statutory_declaration_for_single_applicant,
+                    doc_checker_state_.need_to_send_statutory_declaration_for_single_applicant,
                 need_to_send_statutory_declaration_for_married_applicant=
-                    doc_checker_state.need_to_send_statutory_declaration_for_applicant_in_partnership and
-                    doc_checker_state.is_married,
+                    doc_checker_state_.need_to_send_statutory_declaration_for_applicant_in_partnership and
+                    doc_checker_state_.is_married,
                 need_to_send_statutory_declaration_for_applicant_in_civil_partnership=
-                    doc_checker_state.need_to_send_statutory_declaration_for_applicant_in_partnership and
-                    doc_checker_state.is_in_civil_partnership,
+                    doc_checker_state_.need_to_send_statutory_declaration_for_applicant_in_partnership and
+                    doc_checker_state_.is_in_civil_partnership,
                 need_to_send_spouses_statutory_declaration=
-                    doc_checker_state.need_to_send_partners_statutory_declaration and
-                    doc_checker_state.is_married,
+                    doc_checker_state_.need_to_send_partners_statutory_declaration and
+                    doc_checker_state_.is_married,
                 need_to_send_civil_partners_statutory_declaration=
-                    doc_checker_state.need_to_send_partners_statutory_declaration and
-                    doc_checker_state.is_in_civil_partnership,
+                    doc_checker_state_.need_to_send_partners_statutory_declaration and
+                    doc_checker_state_.is_in_civil_partnership,
                 need_to_send_marriage_certificate=
-                    doc_checker_state.need_to_send_partnership_certificate and
-                    doc_checker_state.is_married,
+                    doc_checker_state_.need_to_send_partnership_certificate and
+                    doc_checker_state_.is_married,
                 need_to_send_civil_partnership_certificate=
-                    doc_checker_state.need_to_send_partnership_certificate and
-                    doc_checker_state.is_in_civil_partnership,
+                    doc_checker_state_.need_to_send_partnership_certificate and
+                    doc_checker_state_.is_in_civil_partnership,
                 need_to_send_death_certificate=
-                    doc_checker_state.need_to_send_death_certificate,
+                    doc_checker_state_.need_to_send_death_certificate,
                 need_to_send_decree_absolute=
-                    doc_checker_state.need_to_send_decree_absolute,
+                    doc_checker_state_.need_to_send_decree_absolute,
                 need_to_send_proof_gender_recognised_outside_uk=
-                    doc_checker_state.need_to_send_proof_gender_recognised_outside_uk
+                    doc_checker_state_.need_to_send_proof_gender_recognised_outside_uk
             )
 
             return local_redirect(url_for('documentChecker.emailSent'))
@@ -224,28 +224,28 @@ def emailSent():
 
 
 def hasUserAnswersAllTheQuestions() -> bool:
-    doc_checker_state = DocCheckerDataStore.load_doc_checker_state()
+    doc_checker_state_ = DocCheckerDataStore.load_doc_checker_state()
 
-    if doc_checker_state.changed_name_to_reflect_gender is None: return False
-    if doc_checker_state.currently_in_a_partnership is None: return False
-    if doc_checker_state.is_currently_in_partnership and doc_checker_state.plan_to_remain_in_a_partnership is None: return False
-    if doc_checker_state.is_not_in_partnership and doc_checker_state.previous_partnership_partner_died is None: return False
-    if doc_checker_state.is_not_in_partnership and doc_checker_state.previous_partnership_ended is None: return False
-    if doc_checker_state.gender_recognition_outside_uk is None: return False
+    if doc_checker_state_.changed_name_to_reflect_gender is None: return False
+    if doc_checker_state_.currently_in_a_partnership is None: return False
+    if doc_checker_state_.is_currently_in_partnership and doc_checker_state_.plan_to_remain_in_a_partnership is None: return False
+    if doc_checker_state_.is_not_in_partnership and doc_checker_state_.previous_partnership_partner_died is None: return False
+    if doc_checker_state_.is_not_in_partnership and doc_checker_state_.previous_partnership_ended is None: return False
+    if doc_checker_state_.gender_recognition_outside_uk is None: return False
     return True
 
 
 def getUrlForNextUnansweredQuestion() -> str:
-    doc_checker_state = DocCheckerDataStore.load_doc_checker_state()
+    doc_checker_state_ = DocCheckerDataStore.load_doc_checker_state()
 
-    if doc_checker_state.changed_name_to_reflect_gender is None: return url_for('documentChecker.previousNamesCheck')
-    if doc_checker_state.currently_in_a_partnership is None: return url_for('documentChecker.currentlyInAPartnership')
-    if doc_checker_state.is_currently_in_partnership and doc_checker_state.plan_to_remain_in_a_partnership is None: return url_for(
+    if doc_checker_state_.changed_name_to_reflect_gender is None: return url_for('documentChecker.previousNamesCheck')
+    if doc_checker_state_.currently_in_a_partnership is None: return url_for('documentChecker.currentlyInAPartnership')
+    if doc_checker_state_.is_currently_in_partnership and doc_checker_state_.plan_to_remain_in_a_partnership is None: return url_for(
         'documentChecker.planToRemainInAPartnership')
-    if doc_checker_state.is_not_in_partnership and doc_checker_state.previous_partnership_partner_died is None: return url_for(
+    if doc_checker_state_.is_not_in_partnership and doc_checker_state_.previous_partnership_partner_died is None: return url_for(
         'documentChecker.previousPartnershipPartnerDied')
-    if doc_checker_state.is_not_in_partnership and doc_checker_state.previous_partnership_ended is None: return url_for(
+    if doc_checker_state_.is_not_in_partnership and doc_checker_state_.previous_partnership_ended is None: return url_for(
         'documentChecker.previousPartnershipEnded')
-    if doc_checker_state.gender_recognition_outside_uk is None: return url_for(
+    if doc_checker_state_.gender_recognition_outside_uk is None: return url_for(
         'documentChecker.genderRecognitionOutsideUK')
     return url_for('documentChecker.your_documents')
