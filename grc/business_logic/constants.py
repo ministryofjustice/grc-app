9f1119975b1922641d177d26301b1cf89fb38858
itoulou <35838791+itoulou@users.noreply.github.com>
2024-03-12 14:47:01 +0000

    Rst 6415 welsh doc checker married p3 (#117)
    * add lazy messages for previous name check and implememnt generic back button
    
    * add generic continue button
    
    * create constants file to reduce duplicate translations
    
    * extract all translations
    
    * update translations
    
    * extract trans for error summary title
    
    * add error message to constants as used more than once
    
    * extract constant error message for trans
    
    * fix same gov template files test
    
    * format validator on same line
    
    * fix same gov template files test
    
    * fix same gov template files test radios.html
    
    * add trans for form question
    
    * RST-6415 mark text for trans married or civil p3
    
    * add translations
    
    * compile message

--- grc/business_logic/constants.py
+++ grc/business_logic/constants.py
@@ -3,6 +3,10 @@ from flask_babel import lazy_gettext as _l
 
 YES = _l('Yes')
 NO = _l('No')
+MARRIED = _l('Married')
+CIVIL_PARTNERSHIP = _l('Civil partnership')
+NEITHER = _l('Neither')
 
 # Error messages
 PREVIOUS_NAME_CHECK_ERROR = _l('Select if you have ever changed your name to reflect your gender')
+CURRENTLY_MARRIED_OR_CIVIL_PARTNERSHIP_ERROR = _l('Select if you are currently married or in a civil partnership')
