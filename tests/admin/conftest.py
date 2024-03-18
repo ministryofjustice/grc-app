95455faae0dd95cf50a87aa5ef49b1cbb6453d47
itoulou <touloumbadjiani@gmail.com>
2024-03-18 17:11:37 +0000

    add unit tests for admin index login page

--- tests/admin/conftest.py
+++ tests/admin/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from admin import create_app
 from admin.config import TestConfig
+from grc.models import db, AdminUser
+from werkzeug.security import generate_password_hash
 
 
 @pytest.fixture()
@@ -11,3 +13,29 @@ def app():
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture()
+def default_admin(app):
+    with app.app_context():
+        test_admin = AdminUser(email=app.config['DEFAULT_ADMIN_USER'],
+                               password=generate_password_hash('123ABC'), userType='ADMIN')
+        db.session.add(test_admin)
+        db.session.commit()
+        yield test_admin
+
+        db.session.delete(test_admin)
+        db.session.commit()
+
+
+@pytest.fixture()
+def admin(app):
+    with app.app_context():
+        test_admin = AdminUser(email=app.config['DEFAULT_ADMIN_USER'], password=generate_password_hash('password'),
+                               userType='ADMIN', passwordResetRequired=False)
+        db.session.add(test_admin)
+        db.session.commit()
+        yield test_admin
+
+        db.session.delete(test_admin)
+        db.session.commit()
