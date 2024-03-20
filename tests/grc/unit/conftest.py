f25b7eeea3723aa782964c1ae73f9f36fba43e73
itoulou <touloumbadjiani@gmail.com>
2024-03-20 10:04:46 +0000

    add ut for is invalid code check

--- tests/grc/unit/conftest.py
+++ tests/grc/unit/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from grc import create_app
 from grc.config import TestConfig
 from grc.models import db, SecurityCode
@@ -29,4 +31,19 @@ def security_code_(app, public_user_email) -> SecurityCode:
             SecurityCode.code == code
         ).first()
         yield security_code_
+
+
+@pytest.fixture()
+def expired_security_code(app, public_user_email):
+    with app.app_context():
+        security_code_ = SecurityCode(code='123456', email=public_user_email)
+        db.session.add(security_code_)
+        now = datetime.now()
+        expired_code_date = now - relativedelta(hours=25)
+        security_code_.created = expired_code_date
+        db.session.commit()
+        yield security_code_
+
+        db.session.delete(security_code_)
+        db.session.commit()
 
--- tests/grc/unit/test_security_code/test_security_code.py
+++ tests/grc/unit/test_security_code/test_security_code.py
@@ -1,5 +1,6 @@
 from grc.models import db, SecurityCode
 from grc.utils import security_code as sc
+from unittest.mock import patch
 
 
 class TestSecurityCode:
@@ -26,4 +27,52 @@ class TestSecurityCode:
 
     def test_is_security_code_valid_invalid_code_public_user(self, app, client, public_user_email):
         with app.app_context():
-            assert sc.is_security_code_valid(public_user_email, 'INVALID CODE', False) is False
+            with app.test_request_context():
+                assert sc.is_security_code_valid(public_user_email, 'INVALID CODE', False) is False
+
+    def test_is_security_code_valid_invalid_code_admin_user(self, app, client, public_user_email):
+        with app.app_context():
+            with app.test_request_context():
+                assert sc.is_security_code_valid(public_user_email, 'INVALID CODE', True) is False
+
+    def test_is_security_code_valid_invalid_email_public_user(self, app, client, public_user_email, security_code_):
+        with app.app_context():
+            with app.test_request_context():
+                assert sc.is_security_code_valid('INVALID EMAIL', security_code_.code, False) is False
+
+    def test_is_security_code_valid_invalid_email_admin_user(self, app, client, public_user_email, security_code_):
+        with app.app_context():
+            with app.test_request_context():
+                assert sc.is_security_code_valid('INVALID EMAIL', security_code_.code, True) is False
+
+    def test_is_security_code_valid_expired_code_public_user(self, app, client, public_user_email,
+                                                             expired_security_code):
+        with app.app_context():
+            with app.test_request_context():
+                assert sc.is_security_code_valid(public_user_email, expired_security_code.code, False) is False
+
+    def test_is_security_code_valid_expired_code_admin_user(self, app, client, public_user_email,
+                                                            expired_security_code):
+        with app.app_context():
+            with app.test_request_context():
+                assert sc.is_security_code_valid(public_user_email, expired_security_code.code, True) is False
+
+    def test_is_security_code_valid_valid_code_admin_user_code_not_deleted(self, app, client, public_user_email,
+                                                                           security_code_):
+        with app.app_context():
+            with app.test_request_context():
+                assert sc.is_security_code_valid(public_user_email, security_code_.code, True) is True
+                user_security_code = SecurityCode.query.filter_by(email=public_user_email,
+                                                                  code=security_code_.code).first()
+                assert user_security_code is not None
+                assert user_security_code.code == security_code_.code
+                assert user_security_code.email == 'test.public.email@example.com'
+
+    @patch('grc.utils.security_code.delete_all_user_codes')
+    def test_is_security_code_valid_valid_code_public_user_code_deleted(self, mock_delete_all_user_codes,
+                                                                        app, client, public_user_email,
+                                                                        security_code_):
+        with app.app_context():
+            with app.test_request_context():
+                assert sc.is_security_code_valid(public_user_email, security_code_.code, False) is True
+                mock_delete_all_user_codes.assert_called_with(public_user_email)
