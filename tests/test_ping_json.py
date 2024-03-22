0af9a0cd1def2a0135c8f8bfcbf9a4ef3dd9a26c
itoulou <touloumbadjiani@gmail.com>
2024-03-22 09:47:10 +0000

    add timeout to ping test to allow waitress to start up

--- tests/test_ping_json.py
+++ tests/test_ping_json.py
@@ -1,18 +1,38 @@
 import os
 import requests
+import time
 
 
-def test_json_health_check():
-    # Access health pages, currently does not work with admin app
-    public_response = requests.get(f"{os.environ['TEST_URL']}/health")
+def pytest_addoption(parser):
+    parser.addoption("--max-retries", type=int, default=1, help="Add number of attempts to ping endpoint")
+    parser.addoption("--timeout", type=int, default=3, help="Timeout in seconds between each ping")
 
-    # Converting into accessible data
-    public_data = public_response.json()
 
-    # Public app assertions
-    assert public_response.status_code == 200
-    assert 'status' in public_data and public_data['status'] == 'success', 'Public app JSON status unsuccessful'
-    assert public_data['results'][0]['passed'] is True, 'Public app JSON results has not passed'
-    print('Public app JSON response is healthy')
+def test_json_health_check(request):
+    max_retries = request.config.getoption("--max-retries")
+    timeout = request.config.getoption("--timeout")
+
+    for attempt in range(1, max_retries + 1):
+        try:
+            response = requests.get(f"{os.environ['TEST_URL']}/health")
+            response.raise_for_status()
+
+            # Converting into accessible data
+            public_data = response.json()
+
+            # Public app assertions
+            assert response.status_code == 200
+            assert 'status' in public_data and public_data['status'] == 'success', 'Public app JSON status unsuccessful'
+            assert public_data['results'][0]['passed'] is True, 'Public app JSON results has not passed'
+            print('Public app JSON response is healthy')
+            return
+
+        except requests.RequestException as e:
+            print(f"Attempt {attempt} failed: {e}")
+            if attempt < max_retries:
+                print(f"Retrying in {timeout} seconds...")
+                time.sleep(timeout)
+
+    print("Failed after maximum retries.")
 
 
