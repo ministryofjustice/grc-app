a169b50e6a91074505f244ccf56f9ce1450226c2
itoulou <touloumbadjiani@gmail.com>
2024-03-25 17:03:06 +0000

    add valid date test and turn condition to guard clause

--- tests/grc/unit/utils/test_custom_form_validators/test_validate_statutory_declaration_date.py
+++ tests/grc/unit/utils/test_custom_form_validators/test_validate_statutory_declaration_date.py
@@ -77,3 +77,20 @@ class Testvalidate_statutory_declaration_date:
                 form.statutory_declaration_date_year.data = f'{statutory_declaration_date_year.year}'
                 with pytest.raises(ValidationError, match='Enter a date that does not precede your transition date'):
                     validate_statutory_declaration_date(form, form.statutory_declaration_date_year)
+
+    @patch('grc.business_logic.data_store.DataStore.load_application')
+    def test_validate_statutory_declaration_date_valid_date(self, mock_load_application, app):
+        with app.app_context():
+            with app.test_request_context():
+                session['reference_number'] = '123ABC'
+
+                mock_transition_date = (datetime.today() - relativedelta(years=3)).date()
+                mock_application_data = MagicMock(personal_details_data=MagicMock(transition_date=mock_transition_date))
+                mock_load_application.return_value = mock_application_data
+
+                statutory_declaration_date_year = datetime.now() - relativedelta(years=2)
+                form = StatutoryDeclarationDateForm()
+                form.statutory_declaration_date_day.data = '4'
+                form.statutory_declaration_date_month.data = '6'
+                form.statutory_declaration_date_year.data = f'{statutory_declaration_date_year.year}'
+                assert validate_statutory_declaration_date(form, form.statutory_declaration_date_year) is None
