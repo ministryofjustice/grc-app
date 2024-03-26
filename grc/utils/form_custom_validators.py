9a7c34f9fcf8c549d4f602d48dd20dd98c8887ac
itoulou <35838791+itoulou@users.noreply.github.com>
2024-03-26 09:46:00 +0000

    Rst 6457 unit test validate reference number (#135)
    * RST-6454 refactor testing project tree for better readability and structure
    
    * run uts and e2e separately in the build
    
    * fix path to upload file helper
    
    * fix circleci indentation
    
    * run smoke test when browser is running in build
    
    * separate health check from e2e
    
    * remove warning from redirect test
    
    * remove depreacted docker version from build
    
    * fix import error for e2e tests
    
    * fix e2e import error
    
    * reroute reports files path
    
    * add timeout to ping test to allow waitress to start up
    
    * use args as fixtures
    
    * remove warnings
    
    * remove deprecation field flags warning
    
    * remove unused validator
    
    * add unit test reference number and refactor function name and return value
    
    * refactor refernece number is valid function as to prevent other email address from accessing application that's not theirs
    
    * split reference is valid func to two for pub and admin best practice

--- grc/utils/form_custom_validators.py
+++ grc/utils/form_custom_validators.py
@@ -7,10 +7,10 @@ from werkzeug.datastructures import FileStorage
 from collections.abc import Iterable
 from datetime import date
 from grc.business_logic.data_store import DataStore
-from grc.utils.security_code import is_security_code_valid
-from grc.utils.reference_number import validate_reference_number
 from grc.models import db, Application
-from grc.utils.logger import Logger, LogLevel
+from grc.utils.security_code import is_security_code_valid
+from grc.utils.reference_number import reference_number_is_valid
+from grc.utils.logger import LogLevel, Logger
 
 logger = Logger()
 
@@ -92,14 +92,12 @@ def validate_security_code(form, field):
         raise ValidationError('Enter the security code that we emailed you')
 
 
-def validateReferenceNumber(form, field):
-    if validate_reference_number(field.data) is False:
-        from grc.utils.logger import LogLevel, Logger
-        logger = Logger()
-        email = logger.mask_email_address(session['validatedEmail']) if 'validatedEmail' in session else 'Unknown user'
+def validate_reference_number(form, field):
+    validated_email = session.get('validatedEmail')
+    if not reference_number_is_valid(field.data, validated_email):
+        email = logger.mask_email_address(validated_email) if validated_email in session else 'Unknown user'
         reference_number = f"{field.data[0: 2]}{'*' * (len(field.data) - 4)}{field.data[-2:]}"
         logger.log(LogLevel.WARN, f"{email} entered an incorrect reference number ({reference_number})")
-
         raise ValidationError('Enter a valid reference number')
 
 
