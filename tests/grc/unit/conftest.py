49c9a554d782eff99aa87e5d11c5e4ee1164b94b
itoulou <35838791+itoulou@users.noreply.github.com>
2024-03-26 09:46:00 +0000

    Rst 6457 unit test validate reference number (#135)
    * RST-6454 refactor testing project tree for better readability and structure
    
    * run uts and e2e separately in the build
    
    * fix path to upload file helper
    
    * fix circleci indentation
    
    * run smoke test when browser is running in build
    
    * separate health check from e2e
    
    * remove warning from redirect test
    
    * remove depreacted docker version from build
    
    * fix import error for e2e tests
    
    * fix e2e import error
    
    * reroute reports files path
    
    * add timeout to ping test to allow waitress to start up
    
    * use args as fixtures
    
    * remove warnings
    
    * remove deprecation field flags warning
    
    * remove unused validator
    
    * add unit test reference number and refactor function name and return value
    
    * refactor refernece number is valid function as to prevent other email address from accessing application that's not theirs
    
    * split reference is valid func to two for pub and admin best practice

--- tests/grc/unit/conftest.py
+++ tests/grc/unit/conftest.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from grc import create_app
 from grc.config import TestConfig
-from grc.models import db, SecurityCode
+from grc.models import db, SecurityCode, Application, ApplicationStatus
 from grc.utils.security_code import security_code_generator
 
 
@@ -47,3 +47,69 @@ def expired_security_code(app, public_user_email):
         db.session.delete(security_code_)
         db.session.commit()
 
+
+@pytest.fixture()
+def test_application(app, public_user_email):
+    with app.app_context():
+        application_record = Application(
+            reference_number='ABCD1234',
+            email=public_user_email
+        )
+        db.session.add(application_record)
+        db.session.commit()
+
+        yield application_record
+
+        db.session.delete(application_record)
+        db.session.commit()
+
+
+@pytest.fixture()
+def test_submitted_application(app, public_user_email):
+    with app.app_context():
+        application_record = Application(
+            reference_number='ABCD1234',
+            email=public_user_email,
+            status=ApplicationStatus.SUBMITTED
+        )
+        db.session.add(application_record)
+        db.session.commit()
+
+        yield application_record
+
+        db.session.delete(application_record)
+        db.session.commit()
+
+
+@pytest.fixture()
+def test_downloaded_application(app, public_user_email):
+    with app.app_context():
+        application_record = Application(
+            reference_number='ABCD1234',
+            email=public_user_email,
+            status=ApplicationStatus.DOWNLOADED
+        )
+        db.session.add(application_record)
+        db.session.commit()
+
+        yield application_record
+
+        db.session.delete(application_record)
+        db.session.commit()
+
+
+@pytest.fixture()
+def test_completed_application(app, public_user_email):
+    with app.app_context():
+        application_record = Application(
+            reference_number='ABCD1234',
+            email=public_user_email,
+            status=ApplicationStatus.COMPLETED
+        )
+        db.session.add(application_record)
+        db.session.commit()
+
+        yield application_record
+
+        db.session.delete(application_record)
+        db.session.commit()
