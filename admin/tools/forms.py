72daae4914325d35da524fccf548c1d4ab546f1e
itoulou <35838791+itoulou@users.noreply.github.com>
2024-03-28 11:54:33 +0000

    Rst 6481 ut multi file allowed (#152)
    * RSt 6481 write ut for admin unlock pdf validator
    
    * add admin app fixture
    
    * remove erroneous redundant check
    
    * set upload set as form attribute for tests to detect future changes
    
    * add tests for public scenarios
    
    * fix validator to loop through all files, potential bug
    
    * refactor validator
    
    * separate admin and public as admin only accepts one file. validator violates SR principle

--- admin/tools/forms.py
+++ admin/tools/forms.py
@@ -1,14 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, MultipleFileField
+from wtforms import PasswordField, FileField
 from wtforms.validators import DataRequired
-from grc.utils.form_custom_validators import MultiFileAllowed, fileSizeLimit, fileVirusScan
+from grc.utils.form_custom_validators import SingleFileAllowed, fileSizeLimit, fileVirusScan
 
 
 class UnlockFileForm(FlaskForm):
-    file = MultipleFileField(
+    upload_set = ['pdf']
+
+    file = FileField(
         validators=[
             DataRequired(message='Select a PDF file to upload'),
-            MultiFileAllowed(['pdf'], message='Select a PDF file to upload'),
+            SingleFileAllowed(upload_set, message='Select a PDF file to upload'),
             fileSizeLimit(10),
             fileVirusScan
         ]
---
+++ tests/grc/unit/utils/test_custom_form_validators/test_validate_multi_files_allowed.py
@@ -0,0 +1,72 @@
+import pytest
+from admin.tools.forms import UnlockFileForm
+from grc.upload.forms import UploadForm
+from grc.utils.form_custom_validators import MultiFileAllowed
+from werkzeug.datastructures import FileStorage
+from unittest.mock import MagicMock
+from wtforms.validators import StopValidation
+
+
+class TestValidateMultiFileAllowed:
+    def test_multi_file_allowed_public_file_upload_valid(self, app):
+        with app.app_context():
+            test_files_uploaded = [
+                FileStorage(filename='test_file1.pdf', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file2.jpg', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file3.jpeg', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file4.png', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file5.tif', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file6.tiff', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file7.bmp', stream=MagicMock(), content_type='text/plain'),
+            ]
+            form = UploadForm()
+            form.documents.data = test_files_uploaded
+            validator = MultiFileAllowed(form.upload_set, 'Select a JPG, BMP, PNG, TIF or PDF file smaller than 10MB')
+            assert validator.__call__(form, form.documents) is None
+
+    def test_multi_file_allowed_public_file_upload_invalid_file_extension_with_message(self, app):
+        with app.app_context():
+            invalid_test_files_uploaded = [
+                FileStorage(filename='test_file1.pdf', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file2.jpg', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file3.jpeg', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file4.pg', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file5.tif', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file6.tiff', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file7.bmp', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file8.invalid_ext', stream=MagicMock(), content_type='text/plain'),
+            ]
+            form = UploadForm()
+            form.documents.data = invalid_test_files_uploaded
+            validator = MultiFileAllowed(form.upload_set, 'Select a JPG, BMP, PNG, TIF or PDF file smaller than 10MB')
+            with pytest.raises(StopValidation, match='Select a JPG, BMP, PNG, TIF or PDF file smaller than 10MB'):
+                validator.__call__(form, form.documents)
+
+    def test_multi_file_allowed_public_file_upload_invalid_file_extension_without_message(self, app):
+        with app.app_context():
+            invalid_test_files_uploaded = [
+                FileStorage(filename='test_file1.pdf', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file2.jpg', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file3.jpeg', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file4.pg', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file5.tif', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file6.tiff', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file7.bmp', stream=MagicMock(), content_type='text/plain'),
+                FileStorage(filename='test_file8.invalid_ext', stream=MagicMock(), content_type='text/plain'),
+            ]
+            form = UploadForm()
+            form.documents.data = invalid_test_files_uploaded
+            validator = MultiFileAllowed(form.upload_set, None)
+            with pytest.raises(StopValidation, match='File does not have an approved extension: jpg, jpeg, png, tif,'
+                                                     ' tiff, bmp, pdf'):
+                validator.__call__(form, form.documents)
+
+    def test_multi_file_allowed_public_file_upload_invalid_file_data_not_iterable(self, app):
+        with app.app_context():
+            invalid_test_files_uploaded = FileStorage(filename='test_file1.pdf', stream=MagicMock(),
+                                                      content_type='text/plain'),
+
+            form = UploadForm()
+            form.documents.data = invalid_test_files_uploaded
+            validator = MultiFileAllowed(form.upload_set, None)
+            assert validator.__call__(form, form.documents) is None
