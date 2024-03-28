f4feaad586132d15dcdf7205f6103484443c9b3c
itoulou <touloumbadjiani@gmail.com>
2024-03-28 14:59:25 +0000

    add its for more emails

--- tests/grc/integration/external_services/test_gov_uk_notify.py
+++ tests/grc/integration/external_services/test_gov_uk_notify.py
@@ -14,7 +14,38 @@ class TestGovNotifyEmails:
 
     def test_send_email_unfinished_application(self, app, public_user_email):
         with app.app_context():
-            response = GovUkNotify().send_email_unfinished_application(public_user_email, '30', 'http://return-link')
-            print(response)
+            response = GovUkNotify().send_email_unfinished_application(public_user_email, '30')
             assert 'Your Gender Recognition Certificate application expires in 30' in response['content']['subject']
-            # assert 'until 12:45 on 29 Mar 2024' in response['content']['body']
+
+    def test_send_email_completed_application(self, app, public_user_email):
+        with app.app_context():
+            response = GovUkNotify().send_email_completed_application(public_user_email, 'list of docs')
+            assert 'Your application has been received' in response['content']['subject']
+            assert 'list of docs' in response['content']['body']
+
+    def test_send_email_feedback(self, app, public_user_email):
+        with app.app_context():
+            response = GovUkNotify().send_email_feedback(
+                public_user_email,
+                'difficult questions to answer',
+                'list of questions which were difficult to answer',
+                'needed to call admin team',
+                'description of what I needed help with',
+                'I used doc checker',
+                'Good experience using doc checker',
+                'No other suggestions'
+            )
+            assert 'Feedback received' in response['content']['subject']
+            assert 'list of questions which were difficult to answer' in response['content']['body']
+            assert 'needed to call admin team' in response['content']['body']
+            assert 'description of what I needed help with' in response['content']['body']
+            assert 'I used doc checker' in response['content']['body']
+            assert 'Good experience using doc checker' in response['content']['body']
+            assert 'No other suggestions' in response['content']['body']
+
+    # @patch('grc.external_services.gov_uk_notify.generate_security_code')
+    # def test_send_email_admin_login_security_code(self, app, public_user_email):
+    #     with app.app_context():
+    #         response = GovUkNotify().send_email_admin_login_security_code(public_user_email)
+    #         assert 'Feedback received' in response['content']['subject']
+    #         assert 'list of questions which were difficult to answer' in response['content']['body']
