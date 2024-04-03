4460876a874cb1ea3bf4b2229665425fe37572e9
itoulou <35838791+itoulou@users.noreply.github.com>
2024-04-03 12:43:06 +0100

    Rst 6484 ut file size limit (#153)
    * RST 6485 add uts for validate file size limit public multi file upload
    
    * refactor validator
    
    * add separate validator for admin form for best practice
    
    * remove unused imports
    
    * rename multi file tests
    
    * add ut for single file size validator
    
    * remove log
    
    * add ut for no data

--- admin/tools/forms.py
+++ admin/tools/forms.py
@@ -1,17 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, FileField
 from wtforms.validators import DataRequired
-from grc.utils.form_custom_validators import SingleFileAllowed, fileSizeLimit, fileVirusScan
+from grc.utils.form_custom_validators import SingleFileAllowed, MultiFileAllowed, validate_file_size_limit, fileVirusScan
 
 
 class UnlockFileForm(FlaskForm):
+    file_size_limit_mb = 10
     upload_set = ['pdf']
 
     file = FileField(
         validators=[
             DataRequired(message='Select a PDF file to upload'),
             SingleFileAllowed(upload_set, message='Select a PDF file to upload'),
-            fileSizeLimit(10),
+            validate_file_size_limit,
             fileVirusScan
         ]
     )
