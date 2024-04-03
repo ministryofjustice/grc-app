6b13ba9de5bf179ed677a1c3d2d4d264e5068772
itoulou <touloumbadjiani@gmail.com>
2024-04-03 09:07:52 +0100

    add test for grc in another country and choice persists in get request

--- tests/grc/integration/start_application/test_overseas_check.py
+++ tests/grc/integration/start_application/test_overseas_check.py
@@ -34,4 +34,34 @@ class TestOverseasCheck:
                 assert after_request_data.confirmation_data.gender_recognition_from_approved_country is None
                 assert after_request_data.confirmation_data.gender_recognition_outside_uk is False
 
+    def test_overseas_check_post_issued_in_another_country_overseas_approved_check_next_page(self, app, client,
+                                                                                             test_application):
+        with app.app_context():
+            with client.session_transaction() as session:
+                session['reference_number'] = test_application.reference_number
+
+            pre_request_data = test_application.application_data()
+            pre_request_data.confirmation_data.gender_recognition_from_approved_country = True
+            save_test_data(pre_request_data)
+            response = client.post('/overseas-check', data={'overseasCheck': True})
+            after_request_data = load_test_data(test_application.reference_number)
+            assert response.status_code == 302
+            assert response.location == '/overseas-approved-check'
+            assert after_request_data.confirmation_data.gender_recognition_from_approved_country is True
+            assert after_request_data.confirmation_data.gender_recognition_outside_uk is True
+
+    def test_overseas_check_get_overseas_check_data_persists(self, app, client, test_application):
+        with app.app_context():
+            with client.session_transaction() as session:
+                session['reference_number'] = test_application.reference_number
+
+            pre_request_data = test_application.application_data()
+            pre_request_data.confirmation_data.gender_recognition_outside_uk = True
+            save_test_data(pre_request_data)
+            response = client.get('/overseas-check')
+            after_request_data = load_test_data(test_application.reference_number)
+            assert response.status_code == 200
+            assert ('Have you ever been issued a Gender Recognition Certificate (or its equivalent)'
+                    ' in another country?') in response.text
+            assert after_request_data.confirmation_data.gender_recognition_outside_uk is True
 
