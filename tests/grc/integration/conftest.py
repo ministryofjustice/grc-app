10339ab95a0ed64c8b21ed12889a164ee158c24d
itoulou <35838791+itoulou@users.noreply.github.com>
2024-04-10 10:33:34 +0100

    Rst 6497 it is first visit (#158)
    * RST 6490 it for start app index
    
    * fix failing i tests
    
    * use different sc generator method for test fixture
    
    * use correct code generator for text fixtures
    
    * rename fixture
    
    * remove try except
    
    * add it for security code public
    
    * send correct email
    
    * mock email sending
    
    * RST 6497 first visit its
    
    * refactor is first visit

--- tests/grc/integration/conftest.py
+++ tests/grc/integration/conftest.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from grc import create_app
 from grc.config import TestConfig
-from grc.models import db, SecurityCode
+from grc.models import db, SecurityCode, Application, ApplicationStatus
 from grc.utils.security_code import generate_security_code_and_expiry
 
 
@@ -45,4 +45,70 @@ def expired_security_code(app, public_user_email):
         yield security_code_
 
         db.session.delete(security_code_)
+        db.session.commit()
+
+
+@pytest.fixture()
+def test_application(app, public_user_email):
+    with app.app_context():
+        application_record = Application(
+            reference_number='ABCD1234',
+            email=public_user_email
+        )
+        db.session.add(application_record)
+        db.session.commit()
+
+        yield application_record
+
+        db.session.delete(application_record)
+        db.session.commit()
+
+
+@pytest.fixture()
+def test_application_no_email(app, public_user_email):
+    with app.app_context():
+        application_record = Application(
+            reference_number='ABCD1234',
+            email=''
+        )
+        db.session.add(application_record)
+        db.session.commit()
+
+        yield application_record
+
+        db.session.delete(application_record)
+        db.session.commit()
+
+
+@pytest.fixture()
+def test_application_deleted(app, public_user_email):
+    with app.app_context():
+        application_record = Application(
+            reference_number='ABCD1234',
+            email=public_user_email,
+            status=ApplicationStatus.DELETED
+        )
+        db.session.add(application_record)
+        db.session.commit()
+
+        yield application_record
+
+        db.session.delete(application_record)
+        db.session.commit()
+
+
+@pytest.fixture()
+def test_application_submitted(app, public_user_email):
+    with app.app_context():
+        application_record = Application(
+            reference_number='ABCD1234',
+            email=public_user_email,
+            status=ApplicationStatus.SUBMITTED
+        )
+        db.session.add(application_record)
+        db.session.commit()
+
+        yield application_record
+
+        db.session.delete(application_record)
         db.session.commit()
