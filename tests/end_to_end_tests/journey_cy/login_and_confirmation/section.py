6272678f1813460c49ad5821e5c2c9f5e56bd39d
itoulou <touloumbadjiani@gmail.com>
2024-04-16 10:38:48 +0100

    add e2e test

--- tests/end_to_end_tests/journey_cy/login_and_confirmation/section.py
+++ tests/end_to_end_tests/journey_cy/login_and_confirmation/section.py
@@ -24,3 +24,23 @@ async def run_checks_on_section(page: Page, asserts: AssertHelpers, helpers: Pag
     # Enter a valid Email Address, click Continue button, see the Security Code page
     await helpers.fill_textbox(field='email', value=data.EMAIL_ADDRESS)
     await helpers.click_button('Parhau')
+
+    # ------------------------------------------------
+    # ---- Security Code page
+    # ------------------------------------------------
+    await asserts.url('/security-code')
+    await asserts.accessibility()
+    await asserts.h1('Rhowch cod diogelwch')
+    await asserts.number_of_errors(0)
+
+    # Change language
+    await asserts.url('/')
+    await asserts.accessibility()
+    await helpers.click_button('English')
+    await asserts.h1('Enter security code')
+    await helpers.click_button('Cymraeg')
+    await asserts.h1('Rhowch cod diogelwch')
+
+    # Enter a valid Security Code, click Continue button
+    await helpers.fill_textbox(field='security_code', value='11111')
+    await helpers.click_button('Parhau')
