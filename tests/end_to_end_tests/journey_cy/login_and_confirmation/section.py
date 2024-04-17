268853bafc50d71fd42852a15c2437dd1496f89f
itoulou <touloumbadjiani@gmail.com>
2024-04-17 16:40:43 +0100

    fix e2e

--- tests/end_to_end_tests/journey_cy/login_and_confirmation/section.py
+++ tests/end_to_end_tests/journey_cy/login_and_confirmation/section.py
@@ -147,7 +147,7 @@ async def run_checks_on_section(page: Page, asserts: AssertHelpers, helpers: Pag
     await asserts.number_of_errors(0)
 
     # Change language
-    await asserts.url('/declaration')
+    await asserts.url('/overseas-approved-check')
     await asserts.accessibility()
     await helpers.click_button('English')
     await asserts.h1('Gender recognition in approved countries and territories')
