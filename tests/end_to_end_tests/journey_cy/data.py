7c5e67f7076b4d9b7781a6661477ae9320f6795e
itoulou <touloumbadjiani@gmail.com>
2024-04-22 10:17:15 +0100

    add test

--- tests/end_to_end_tests/journey_cy/data.py
+++ tests/end_to_end_tests/journey_cy/data.py
@@ -1,3 +1,8 @@
 DEFAULT_TIMEOUT = 3 * 1000  # Wait a maximum of 3 seconds
 
 EMAIL_ADDRESS = 'test.public.email@example.com'
+
+TITLE = 'Mr'
+FIRST_NAME = 'Joseph'
+MIDDLE_NAMES = 'Adam Brian'
+LAST_NAME = 'Bloggs'
