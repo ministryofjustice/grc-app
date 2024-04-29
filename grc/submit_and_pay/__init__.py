0c864857bbea69a9c78bf2b5b38c2295c6439391
itoulou <35838791+itoulou@users.noreply.github.com>
2024-04-29 15:55:44 +0100

    Rst 6579 ut application files 2 (#187)
    * RST 6524 initial commit prep test data for unit tests
    
    * add app files helpers to generate test uploads data
    
    * add app files helpers to generate test uploads data
    
    * refactor upload helper and tests for getting docs with section
    
    * start ut for create or download and add test data files and mock s3 client
    
    * remove unused import and fixture
    
    * mock upload file s3 client in test upload helper
    
    * add logs to create or download attachments
    
    * ignore tests temp
    
    * revert config fixture change
    
    * revert admin download check condition
    
    * amend mock download object and save method calls input
    
    * add test to check correct files are downloaded
    
    * create test aws client helper
    
    * assert download objects called including original file names
    
    * rename test
    
    * add logging
    
    * revert application files util to mirror staging
    
    * test new create zip function
    
    * add test for uploading zip file to s3
    
    * begin writing test for download attachment
    
    * amend mock s3 client to cater for upload and no zip file exists
    
    * amend return type for download attachments
    
    * add test for download zip doesn't exist
    
    * run tests in build
    
    * fix tests for get files for section tests
    
    * mark zip files method as internal
    
    * mark zip files method as internal
    
    * amend submission and download attachments to use correct methods
    
    * remove now redundandt create_or_download method and mark get methods as internal
    
    * RST-6579 refactor create or download util into more legible methods
    
    * amend old usage of old util to use new specific utils
    
    * amend old usage of old util to use new specific utils
    
    * reorder internal methods
    
    * add tests for all new aspects of pdf download and upload
    
    * assert mock upload function call with args

--- grc/submit_and_pay/__init__.py
+++ grc/submit_and_pay/__init__.py
@@ -155,14 +155,9 @@ def checkYourAnswers():
 def download():
     application_data = DataStore.load_application_by_session_reference_number()
 
-    bytes, file_name = ApplicationFiles().create_or_download_pdf(
-        application_data.reference_number,
-        application_data,
-        is_admin=False,
-        download=True
-    )
+    bytes_, file_name = ApplicationFiles().create_pdf_public(application_data)
 
-    response = make_response(bytes)
+    response = make_response(bytes_)
     response.headers.set('Content-Type', 'application/pdf')
     response.headers.set('Content-Disposition', 'attachment', filename=file_name)
     return response
@@ -207,16 +202,10 @@ def confirmation():
 
     @copy_current_request_context
     def create_files(reference_number, application_data_):
-        ApplicationFiles().create_and_upload_attachments(
-            reference_number,
-            application_data_,
-        )
-        ApplicationFiles().create_or_download_pdf(
-            reference_number,
-            application_data_
-        )
-
-        mark_files_created(reference_number)
+        app_files_util = ApplicationFiles()
+        if app_files_util.create_and_upload_attachments(reference_number, application_data_) and \
+                app_files_util.upload_pdf_admin_with_file_names_attached(application_data_):
+            mark_files_created(reference_number)
 
     threading.Thread(target=create_files, args=[application_data.reference_number, application_data]).start()
 
