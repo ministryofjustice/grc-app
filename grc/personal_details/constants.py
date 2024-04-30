0cf18503b17443ebdc127cc445eb856cbfd11ce2
itoulou <35838791+itoulou@users.noreply.github.com>
2024-04-30 10:12:55 +0100

    Rst 6567 welsh pers details name (#178)
    * RST-6567 mark content for trans
    
    * copy continue changes to other app templates
    
    * translate content
    
    * add test

---
+++ grc/personal_details/constants.py
@@ -0,0 +1,8 @@
+from flask_babel import lazy_gettext as _l
+
+
+class PersonalDetailsConstants:
+    # Error messages
+    TITLE_ERROR = _l('Enter your title')
+    FIRST_NAME_ERROR = _l('Enter your first name(s)')
+    LAST_NAME_ERROR = _l('Enter your last name')
