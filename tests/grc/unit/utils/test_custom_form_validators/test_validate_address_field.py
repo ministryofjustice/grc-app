fe4aea84597f37c9df66c1a024ab7a313af5b1e7
itoulou <touloumbadjiani@gmail.com>
2024-05-01 16:54:22 +0100

    write ut for default error message value

--- tests/grc/unit/utils/test_custom_form_validators/test_validate_address_field.py
+++ tests/grc/unit/utils/test_custom_form_validators/test_validate_address_field.py
@@ -1,10 +1,20 @@
 import pytest
+from flask_wtf import FlaskForm
 from grc.personal_details.forms import AddressForm
 from grc.utils.form_custom_validators import validate_address_field
+from wtforms.fields import StringField
 from wtforms.validators import ValidationError
 
 
 class TestValidateAddressField:
+
+    @pytest.fixture
+    def test_form(self, app):
+        with app.app_context():
+            class TestForm(FlaskForm):
+                test_field = StringField()
+
+            return TestForm()
 
     def test_validate_address_field_valid(self, app):
         with app.app_context():
@@ -35,3 +45,9 @@ class TestValidateAddressField:
                 with pytest.raises(ValidationError, match='Enter a valid address line two'):
                     validate_address_field(form, form.address_line_two)
 
+    def test_validate_address_field_invalid_field_default_error(self, app, test_form):
+        with app.app_context():
+            test_form.test_field.data = '12 in^al(id a££ress line'
+            with app.test_request_context():
+                with pytest.raises(ValidationError, match='Enter your address'):
+                    validate_address_field(test_form, test_form.test_field)
