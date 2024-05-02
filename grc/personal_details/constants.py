347bda31a8cb7d1a693787f31c6603d53b02a651
itoulou <35838791+itoulou@users.noreply.github.com>
2024-05-02 17:24:12 +0100

    Rst 6608 welsh pers dets contact dates (#194)
    * RST-6608 mark content for translation
    
    * mark content with missing translations
    
    * mark content for trans correctly
    
    * ament gov templates and refactor template sync test
    
    * add translations
    
    * add e2e test
    
    * fix failing personal_details/test_contact_dates_form ut
    
    * fix failing validate date ranges and single date uts
    
    * fix remaining tests
    
    * remove outdated translation
    
    * add missing trans for error message

--- grc/personal_details/constants.py
+++ grc/personal_details/constants.py
@@ -5,9 +5,13 @@ from grc.business_logic.constants import BaseConstants
 class PersonalDetailsConstants(BaseConstants):
     MALE = _l('Male')
     FEMALE = _l('Female')
+    SINGLE_DATE = _l('A single date')
+    DATE_RANGE = _l('A range of dates')
+    NO_DATES = _l('No dates')
 
     # Error messages
     TITLE_ERROR = _l('Enter your title')
     FIRST_NAME_ERROR = _l('Enter your first name(s)')
     LAST_NAME_ERROR = _l('Enter your last name')
     NO_AFFIRMED_GENDER_ERROR = _l('Select your affirmed gender')
+    NO_CONTACT_DATES_OPTION_ERROR = _l("Select if you don't want us to contact you at any point in the next 6 months")
