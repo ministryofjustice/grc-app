04238492e12ecf3aabbb03457c4fe7addd26e448
itoulou <35838791+itoulou@users.noreply.github.com>
2024-05-13 16:30:39 +0100

    Rst 6639 welsh upload overseas (#214)
    * RST-6636 mark content for trans
    
    * fix macros
    
    * add translations
    
    * remove whitespace
    
    * RST-6637 mark content for trans
    
    * translate content
    
    * add e2e tes
    
    * fix test
    
    * fix test
    
    * fix translations
    
    * RST-6639 mark content for trans
    
    * translate content
    
    * add e2e
    
    * amend comments in test
    
    * fix test

--- tests/end_to_end_tests/journey_cy/uploads/section.py
+++ tests/end_to_end_tests/journey_cy/uploads/section.py
@@ -1,10 +1,16 @@
 from playwright.async_api import Page
 from tests.end_to_end_tests.helpers.e2e_assert_helpers import AssertHelpers
 from tests.end_to_end_tests.helpers.e2e_page_helpers import PageHelpers
+import tests.end_to_end_tests.journey_cy.uploads.page_overseas_documents as page_overseas_documents
 import tests.end_to_end_tests.journey_cy.uploads.page_statutory_declarations as page_statutory_declarations
 
 
 async def run_checks_on_section(page: Page, asserts: AssertHelpers, helpers: PageHelpers):
+
+    # ------------------------------------------------
+    # ---- Overseas Documents page
+    # ------------------------------------------------
+    await page_overseas_documents.run_checks_on_page(page, asserts, helpers)
 
     # ------------------------------------------------
     # ---- Statutory Declarations page
