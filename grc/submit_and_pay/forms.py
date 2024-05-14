6261869132c95dddbca0cb64b1b2d464d13f1afa
itoulou <35838791+itoulou@users.noreply.github.com>
2024-05-14 16:22:36 +0100

    Rst 6645 welsh subpay index (#217)
    * RST-6645 mark content for translation
    
    * translate content
    
    * add e2e

--- grc/submit_and_pay/forms.py
+++ grc/submit_and_pay/forms.py
@@ -1,17 +1,20 @@
 from flask_wtf import FlaskForm
+from grc.business_logic.data_structures.submit_and_pay_data import HelpWithFeesType
+from grc.submit_and_pay.constants import SubmitAndPayConstants as c
+from grc.lazy.lazy_fields import LazyRadioField
+from grc.lazy.lazy_form_custom_validators import LazyDataRequired
+from grc.utils.form_custom_validators import StrictRequiredIf, validate_hwf_reference_number
 from wtforms import StringField, RadioField, BooleanField
 from wtforms.validators import DataRequired
-from grc.business_logic.data_structures.submit_and_pay_data import HelpWithFeesType
-from grc.utils.form_custom_validators import StrictRequiredIf, validate_hwf_reference_number
 
 
 class MethodCheckForm(FlaskForm):
-    applying_for_help_with_fee = RadioField(
-        choices=[
-            (True, 'Yes'),
-            (False, 'No, I will pay now')
+    applying_for_help_with_fee = LazyRadioField(
+        lazy_choices=[
+            (True, c.YES),
+            (False, c.NO_PAY_NOW)
         ],
-        validators=[DataRequired(message='Select if you are applying for help paying the fee')]
+        validators=[LazyDataRequired(lazy_message=c.HWF_ERROR)]
     )
 
 
