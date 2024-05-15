cbf4cb76c21e152cb5b9587b5db612502008f684
itoulou <touloumbadjiani@gmail.com>
2024-05-15 11:15:41 +0100

    remove constants files to be moved to business logic

--- grc/start_application/constants.py
+++
@@ -1,7 +0,0 @@
-from flask_babel import lazy_gettext as _l
-from grc.business_logic.constants import BaseConstants
-
-
-class StartApplicationConstants(BaseConstants):
-    YES_WITH_REFERENCE_NUMBER = _l('Yes, and I have my reference number')
-    YES_LOST_REFERENCE_NUMBER = _l('Yes, but I have lost my reference number')
