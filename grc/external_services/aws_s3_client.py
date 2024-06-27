ed6959118903fd477aa38c0de58eb687a4b529c3
itoulou <touloumbadjiani@gmail.com>
2024-06-27 16:23:45 +0100

    undi

--- grc/external_services/aws_s3_client.py
+++ grc/external_services/aws_s3_client.py
@@ -82,9 +82,9 @@ class AwsS3Client:
 
     def download_object(self, object_name):
         logger.log(LogLevel.INFO, f"Downloading {object_name}")
-        data = None
-        bytes_buffer = io.BytesIO()
+
         try:
+            bytes_buffer = io.BytesIO()
             self.s3.download_fileobj(Bucket=self.bucket_name, Key=object_name, Fileobj=bytes_buffer)
             data = bytes_buffer
 
@@ -92,8 +92,6 @@ class AwsS3Client:
             logging.error(e)
             logger.log(LogLevel.ERROR, message=f'{e}')
             data = None
-        finally:
-            bytes_buffer.close()
 
         return data
 
