13c8c3dec43a2e1f49a4b0cffc6679fd6ae43a2c
itoulou <35838791+itoulou@users.noreply.github.com>
2024-07-22 13:47:19 +0100

    Rst 6781 prod nightly fix (#239)
    * RST-6781 prod nightly add new tests for job
    
    * remove old test
    
    * dont use custom error handler on grc and admin if on dev or local
    
    * wrap getting of data in try except to catch error
    
    * seaprate tests out and run respective jobs
    
    * move tests to respective job tests
    
    * create new job files and move job code from notify job to respective job files
    
    * remove code unrelated to this job
    
    * add job blueprints
    
    * add new job manifests
    
    * update imports in failing test
    
    * amend pipeline to update all jobs
    
    * log application notified count in job
    
    * add more informative logs
    
    * change job schedule to run nightly
    
    * add test user not notified if already have submitted app
    
    * format code
    
    * format code
    
    * run anonymise job every 10 mins for testing
    
    * revert schedule of anonymis apps job
    
    * upgrade xhtml2pdf
    
    * revert
    
    * debug db migration step
    
    * remove unused imports
    
    * pin python-bidi to last known stable version so not to break xhtml2pdf
    
    * remigrate the db
    
    * stagger the jobs
    
    * add jobs for staging and prod

---
+++ admin/jobs/delete_completed_applications.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from flask import Blueprint
+from flask.cli import with_appcontext
+from grc.models import db, Application, ApplicationStatus
+from grc.utils.application_files import ApplicationFiles
+
+delete_completed_applications = Blueprint('delete_completed_applications', __name__)
+
+
+def mark_applications_as_deleted():
+    print(f'\nDeleting completed applications', flush=True)
+    days_between_application_completion_and_anonymisation = 7
+
+    now = datetime.now()
+    earliest_allowed_application_completed_date = calculate_earliest_allowed_application_completed_date(now, days_between_application_completion_and_anonymisation)
+
+    applications_to_delete = Application.query.filter(
+        Application.status == ApplicationStatus.COMPLETED,
+        Application.completed < earliest_allowed_application_completed_date
+    )
+    print(f'Deleting {applications_to_delete.count()} completed applications\n', flush=True)
+    all_applications_to_delete = applications_to_delete.all()
+    for application_to_delete in all_applications_to_delete:
+        delete_application(application_to_delete)
+    db.session.commit()
+
+    return 200
+
+
+def calculate_earliest_allowed_application_completed_date(now, days_between_application_completion_and_anonymisation):
+    return now - relativedelta(days=days_between_application_completion_and_anonymisation)
+
+
+def delete_application(application_to_delete):
+    if not application_to_delete:
+        return
+
+    try:
+        data = application_to_delete.application_data()
+    except Exception as e:
+        print(f'Error getting application data for {application_to_delete.reference_number} - {e}', flush=True)
+        data = None
+
+    try:
+        if data:
+            ApplicationFiles().delete_application_files(
+                application_to_delete.reference_number,
+                data,
+            )
+
+            application_to_delete.email = ''
+            application_to_delete.user_input = ''
+    except Exception as e:
+        print(f'Error deleting application files for {application_to_delete.reference_number} - {e}', flush=True)
+    finally:
+        application_to_delete.status = ApplicationStatus.DELETED
+
+
+@delete_completed_applications.cli.command('run')
+@with_appcontext
+def main():
+    try:
+        print('running delete completed apps job', flush=True)
+        applications_deleted = mark_applications_as_deleted()
+        assert applications_deleted == 200
+        print('finished delete completed apps job', flush=True)
+    except Exception as e:
+        print(f'Error delete completed apps cron, message = {e}', flush=True)
+
+
+if __name__ == '__main__':
+    main()
