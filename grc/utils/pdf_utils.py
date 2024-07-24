2cfc90757a25e7dee3516e5cd9788a10d48c5afd
Mark Butler <mgb777@outlook.com>
2024-07-24 15:56:58 +0100

    fine tune pdf margins remove bullet list colour formatting remove top margin for pdf title now we can control pdf margins

--- grc/utils/pdf_utils.py
+++ grc/utils/pdf_utils.py
@@ -27,7 +27,7 @@ class PDFUtils():
         pdf_stream: BytesIO = BytesIO()
         data = pdfkit.from_string(
             html,
-            options={"enable-local-file-access": "", "cache-dir": "/tmp", "margin-top": "15", "margin-bottom": "15"},
+            options={"enable-local-file-access": "", "cache-dir": "/tmp", "margin-top": "12", "margin-bottom": "12"},
             css=css,
             verbose=True
         )
