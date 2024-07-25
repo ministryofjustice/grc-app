c5e6031c0bfbdefe5b8375c77f6464ed9a7c2845
Mark Butler <mgb777@outlook.com>
2024-07-25 16:28:55 +0100

    reduce page margin to 8 remove govuk--body template from download.html re-implement list--bullet>li::marker

--- grc/utils/pdf_utils.py
+++ grc/utils/pdf_utils.py
@@ -27,7 +27,7 @@ class PDFUtils():
         pdf_stream: BytesIO = BytesIO()
         data = pdfkit.from_string(
             html,
-            options={"enable-local-file-access": "", "cache-dir": "/tmp", "margin-top": "10", "margin-bottom": "10"},
+            options={"enable-local-file-access": "", "cache-dir": "/tmp", "margin-top": "8", "margin-bottom": "8"},
             css=css,
             verbose=True
         )
