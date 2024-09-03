aaf68b402ff2f77252c6c5823fda3b8470451c01
sgmselli <sellings.matthew@gmail.com>
2024-09-03 16:19:11 +0100

    Replace print statements in admin/jobs/anonymise_expired_applications.py with ERROR & INFO loggers

--- admin/jobs/anonymise_expired_applications.py
+++ admin/jobs/anonymise_expired_applications.py
@@ -4,13 +4,15 @@ from flask import Blueprint
 from flask.cli import with_appcontext
 from grc.models import db, Application, ApplicationStatus
 from grc.utils.application_files import ApplicationFiles
+from grc.utils.logger import LogLevel, Logger
 
+logger = Logger()
 anonymise_expired_applications = Blueprint('anonymise_expired_applications', __name__)
 
 
 def abandon_application_after_period_of_inactivity():
     days_between_last_update_and_deletion = 183  # approximately 6 months
-    print(f'\nAbandoning applications {days_between_last_update_and_deletion} days from last update\n', flush=True)
+    logger.log( LogLevel.INFO, f'\nAbandoning applications {days_between_last_update_and_deletion} days from last update\n')
 
     now = datetime.now()
     earliest_allowed_inactive_application_updated_date = calculate_earliest_allowed_inactive_application_updated_date(now, days_between_last_update_and_deletion)
@@ -20,7 +22,7 @@ def abandon_application_after_period_of_inactivity():
         Application.updated < earliest_allowed_inactive_application_updated_date
     )
 
-    print(f'Anonymising {applications_to_anonymise.count()} inactive applications', flush=True)
+    logger.log(LogLevel.INFO,f'Anonymising {applications_to_anonymise.count()} inactive applications')
     for application_to_anonymise in applications_to_anonymise.all():
         anonymise_application(application_to_anonymise)
 
@@ -40,7 +42,7 @@ def anonymise_application(application_to_anonymise):
     try:
         data = application_to_anonymise.application_data()
     except Exception as e:
-        print(f'Error getting application data for {application_to_anonymise.reference_number} - {e}', flush=True)
+        logger.log(LogLevel.ERROR, f'Error getting application data for {application_to_anonymise.reference_number} - {e}')
         data = None
 
     try:
@@ -53,7 +55,7 @@ def anonymise_application(application_to_anonymise):
             application_to_anonymise.email = ''
             application_to_anonymise.user_input = ''
     except Exception as e:
-        print(f'Error deleting application files for {application_to_anonymise.reference_number} - {e}', flush=True)
+        logger.log(LogLevel.ERROR, f'Error deleting application files for {application_to_anonymise.reference_number} - {e}')
     finally:
         application_to_anonymise.status = ApplicationStatus.ABANDONED
 
@@ -62,12 +64,13 @@ def anonymise_application(application_to_anonymise):
 @with_appcontext
 def main():
     try:
-        print('running anonymise expired applications job', flush=True)
+        logger.log(LogLevel.INFO,'running anonymise expired applications job')
         anonymised_applications = abandon_application_after_period_of_inactivity()
         assert anonymised_applications == 200
-        print('finished anonymise expired applications job', flush=True)
+        logger.log(LogLevel.INFO,'finished anonymise expired applications job')
     except Exception as e:
-        print(f'Error anonymise expired applications cron, message = {e}', flush=True)
+        logger.log(LogLevel.ERROR,f'Error anonymise expired applications cron, message = {e}')
+
 
 
 if __name__ == '__main__':
