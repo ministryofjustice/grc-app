28d86cfae5d892237e727df4e4c074f7599a8de0
sgmselli <sellings.matthew@gmail.com>
2024-09-03 16:21:25 +0100

    Replace print statements in admin/jobs/notify_applicants_inactive_apps.oy with ERROR & INFO loggers

--- admin/jobs/notify_applicants_inactive_apps.py
+++ admin/jobs/notify_applicants_inactive_apps.py
@@ -5,9 +5,10 @@ from flask.cli import with_appcontext
 from sqlalchemy.sql import extract, not_
 from grc.external_services.gov_uk_notify import GovUkNotify
 from grc.models import Application, ApplicationStatus
+from grc.utils.logger import LogLevel, Logger
 
 notify_applicants_inactive_apps = Blueprint('notify_applicants_inactive_apps', __name__)
-
+logger = Logger()
 
 def send_reminder_emails_before_application_deletion():
     days_between_last_update_and_deletion = 183
@@ -21,7 +22,7 @@ def send_reminder_emails_before_application_deletion():
 
     for days_to_send_reminder, time_phrase in deletion_reminder_days_and_phrases.items():
 
-        print(f'Sending reminder emails to applications {time_phrase} from being deleted', flush=True)
+        logger.log(LogLevel.INFO, f'Sending reminder emails to applications {time_phrase} from being deleted')
 
         today = datetime.today()
         last_updated_date = calculate_last_updated_date(today, days_to_send_reminder, days_between_last_update_and_deletion)
@@ -33,7 +34,8 @@ def send_reminder_emails_before_application_deletion():
             extract('year', Application.updated) == last_updated_date.year
         )
 
-        print(f'Sending reminder emails to {applications_to_remind.count()} applications', flush=True)
+        logger.log(LogLevel.INFO, f'Sending reminder emails to {applications_to_remind.count()} applications')
+
         email_send_count = 0
         for application_to_remind in applications_to_remind.all():
             existing_application = Application.query.filter(
@@ -46,7 +48,7 @@ def send_reminder_emails_before_application_deletion():
                     expiry_days=time_phrase
                 )
                 email_send_count += 1
-        print(f'Send {email_send_count} emails\n', flush=True)
+        logger.log(LogLevel.INFO, f'Send {email_send_count} emails\n')
 
     return 200
 
@@ -59,13 +61,12 @@ def calculate_last_updated_date(today, days_to_send_reminder_before_deletion, da
 @with_appcontext
 def main():
     try:
-        print('running notify applicants inactive apps job', flush=True)
+        logger.log(LogLevel.INFO, 'running notify applicants inactive apps job')
         applicants_notified = send_reminder_emails_before_application_deletion()
         assert applicants_notified == 200
-        print('finished notify applicants inactive apps job', flush=True)
+        logger.log(LogLevel.INFO, 'finished notify applicants inactive apps job')
     except Exception as e:
-        print(f'Error notifying applicants cron, message = {e}', flush=True)
-
+        logger.log(LogLevel.ERROR, f'Error notifying applicants cron, message = {e}')
 
 if __name__ == '__main__':
     main()
