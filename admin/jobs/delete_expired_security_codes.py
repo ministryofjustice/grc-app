038e8f631b5f7b6adbc4a7f94b7fe6305e243351
sgmselli <sellings.matthew@gmail.com>
2024-09-04 16:56:31 +0100

    revert changes for a test files

--- admin/jobs/delete_expired_security_codes.py
+++ admin/jobs/delete_expired_security_codes.py
@@ -3,14 +3,12 @@ from dateutil.relativedelta import relativedelta
 from flask import Blueprint
 from flask.cli import with_appcontext
 from grc.models import db, SecurityCode
-from grc.utils.logger import LogLevel, Logger
 
-logger = Logger()
 delete_expired_security_codes = Blueprint('delete_expired_security_codes', __name__)
 
 
 def delete_security_codes():
-    logger.log(LogLevel.WARN, f'\nDeleting expired security codes')
+    print(f'\nDeleting expired security codes')
     hours_between_security_code_creation_and_expiry = 24
 
     now = datetime.now()
@@ -20,7 +18,7 @@ def delete_security_codes():
         SecurityCode.created < earliest_allowed_security_code_creation_time
     )
 
-    logger.log(LogLevel.WARN, f'Deleting {security_codes_to_delete.count()} expired security codes\n')
+    print(f'Deleting {security_codes_to_delete.count()} expired security codes\n')
 
     for security_code_to_delete in security_codes_to_delete:
         db.session.delete(security_code_to_delete)
@@ -38,12 +36,12 @@ def calculate_earliest_allowed_security_code_creation_time(now, hours_between_se
 @with_appcontext
 def main():
     try:
-        logger.log(LogLevel.WARN, 'running delete expired security codes job')
+        print('running delete expired security codes job')
         security_codes_deleted = delete_security_codes()
         assert security_codes_deleted == 200
-        logger.log(LogLevel.INFO, 'finished delete expired security codes job')
+        print('finished delete expired security codes job')
     except Exception as e:
-        logger.log(LogLevel.ERROR, f'Error delete expired security codes cron, message = {e}')
+        print(f'Error delete expired security codes cron, message = {e}')
 
 
 if __name__ == '__main__':
