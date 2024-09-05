8d0119eb42abb7be2cfb3bff5c09c090192cf42e
sgmselli <sellings.matthew@gmail.com>
2024-09-05 11:43:01 +0100

    revert out of scope changes

--- setup-db.py
+++ setup-db.py
@@ -1,11 +1,12 @@
 import psycopg2
 
+
 conn = psycopg2.connect(
     database='postgres',
     user='postgres',
     password='password',
     host='localhost',
-    port='5432'
+    port= '5432'
 )
 conn.autocommit = True
 cursor = conn.cursor()
