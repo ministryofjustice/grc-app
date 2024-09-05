d11574f316bdcec4604c673275d888ebaeea69b6
sgmselli <sellings.matthew@gmail.com>
2024-09-05 11:54:26 +0100

    Revert "Remove unused print statements"
    This reverts commit 1c1a4454ae784fb7df72e4596cbb7240dda3676f.

--- grc/utils/filters.py
+++ grc/utils/filters.py
@@ -9,6 +9,7 @@ from grc.external_services.aws_s3_client import AwsS3Client
 
 blueprint = flask.Blueprint('filters', __name__)
 
+
 @jinja2.pass_context
 @blueprint.app_template_filter('format_date')
 def format_date_filter(context, dt):
@@ -77,10 +78,13 @@ def is_FieldList(context, value):
 @jinja2.pass_context
 @blueprint.app_template_filter('image_data')
 def image_data_filter(context, image_name):
+    print('image_data_filter', flush=True)
+    print(image_name, flush=True)
     if image_name:
         data, width, height = AwsS3Client().download_object_data(image_name)
         return data
     return ''
+
 
 @jinja2.pass_context
 @blueprint.app_template_filter('image_width')
