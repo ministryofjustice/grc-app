ad7a5a3077617934fefe5f7f851c68c1e6da5e3e
sgmselli <sellings.matthew@gmail.com>
2024-11-11 16:15:54 +0000

    Resolve issue with menu bar due to govuk-frontend-5.7.§.min.js not rendering js. Change Text to lower case. Add service navigation.

--- grc/list_status.py
+++ grc/list_status.py
@@ -3,9 +3,9 @@ from flask_babel import lazy_gettext as _l
 
 
 class ListStatus(enum.Enum):
-    COMPLETED = _l("COMPLETED")
-    IN_PROGRESS = _l("IN PROGRESS")
-    NOT_STARTED = _l("NOT STARTED")
-    CANNOT_START_YET = _l("CANNOT START YET")
-    IN_REVIEW = _l("IN REVIEW")  # Value 'in progress' is used only at task list
-    ERROR = _l("ERROR")
+    COMPLETED = _l("Completed")
+    IN_PROGRESS = _l("In progress")
+    NOT_STARTED = _l("Not started")
+    CANNOT_START_YET = _l("Cannot start yet")
+    IN_REVIEW = _l("In review")  # Value 'in progress' is used only at task list
+    ERROR = _l("Error")
