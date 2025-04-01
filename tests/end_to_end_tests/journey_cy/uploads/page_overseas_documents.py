7ec505eeded0ad5e4cc284fe973a6eb6db6fa536
Matthew Sellings <99349293+sgmselli@users.noreply.github.com>
2025-04-01 11:06:00 +0100

    Rst 7122 upload document content update (#310)

--- tests/end_to_end_tests/journey_cy/uploads/page_overseas_documents.py
+++ tests/end_to_end_tests/journey_cy/uploads/page_overseas_documents.py
@@ -7,7 +7,7 @@ import tests.end_to_end_tests.journey_1.data as data
 TASK_LIST_BUTTON_NAME = 'Dogfennau Tystysgrif o Dramor'
 PAGE_URL = '/upload/overseas-certificate'
 PAGE_H1_EN = 'Upload overseas gender recognition certificate documents'
-PAGE_H1_CY = 'Dogfennau tystysgrif cydnabod rhywedd tramor'
+PAGE_H1_CY = 'Uwchlwytho dogfennau tystysgrif cydnabod rhywedd tramor'
 
 
 async def run_checks_on_page(page: Page, asserts: AssertHelpers, helpers: PageHelpers):
