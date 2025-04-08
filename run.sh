bc8bc3ffcbb4ee7ac82ea0de4ccf18e3380cada4
sgmselli <sellings.matthew@gmail.com>
2025-04-08 12:06:18 +0100

    Add migration commands in run.sh file so migrations apply

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "flask db commands commented out to bypass db migration"
-#flask db init
-#flask db migrate
-#flask db upgrade
+flask db init
+flask db migrate
+flask db upgrade
 #flask run --host=0.0.0.0 -p 3000
 
 waitress-serve --call --port=3000 'grc:create_app'
