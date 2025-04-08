8f129226ec0f468b7290e55ff1e1a4d3ec653602
Matthew Sellings <99349293+sgmselli@users.noreply.github.com>
2025-04-08 10:16:13 +0100

    Rst 7235 documents url for glimr (#306)
    * Create documents url in ApplicationData class and use as GLiMR parameter in GlimrNewCase class to send to GLiMR API
    
    * Remove contact plan
    
    * Fix upload of application pdf to s3 bucket on confirmation page
    
    * DEBUG: partially fix test causing issue
    
    * Change pdf upload to include files
    
    * Refactor code so testing easier

--- admin/glimr/glimr_new_case.py
+++ admin/glimr/glimr_new_case.py
@@ -55,6 +55,7 @@ class GlimrNewCase:
         return {
                 'jurisdictionId':self.jurisdiction_id,
                 'onlineMappingCode':self.online_mapping_code,
+                'documentsUrl': self.application_data.documents_url(),
                 **self.case_params(),
                 **self.contact_params(),
                 **self.contact_street()
@@ -66,7 +67,6 @@ class GlimrNewCase:
         """
         return {
             'caseType': self.get_case_type(),
-            'contactPlan': self.get_contact_plan(),
             'dateReceived': self.get_date_received(),
             'dateRegistered': self.get_date_registered(),
             'track': self.track
@@ -105,14 +105,6 @@ class GlimrNewCase:
             return 'Standard Application'
         else:
             return 'Overseas Application'
-
-    def get_contact_plan(self) -> str:
-        if self.partnership_details.is_in_civil_partnership:
-            return 'Applicant + Civil Partner'
-        elif self.partnership_details.is_married:
-            return 'Applicant + Spouse'
-        else:
-            return 'Applicant Only'
 
     def get_date_received(self) -> str:
         return self.format_date(self.application.updated)
