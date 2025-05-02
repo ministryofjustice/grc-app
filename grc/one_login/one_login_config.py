5a0f5ecccdd095c0bdef88062d8277ddc5e0c919
sgmselli <sellings.matthew@gmail.com>
2025-05-02 16:14:21 +0100

    One login prove identity route and logout

--- grc/one_login/one_login_config.py
+++ grc/one_login/one_login_config.py
@@ -1,13 +1,16 @@
 from flask import current_app
 import requests
 from typing import Dict, Any
-from cryptography.hazmat.primitives import serialization
+from functools import lru_cache
 
 class OneLoginConfig:
 
     def __init__(self):
         self.client_id: str = current_app.config['ONE_LOGIN_CLIENT_ID']
-        self.redirect_uri: str = current_app.config['ONE_LOGIN_REDIRECT_URI']
+        self.auth_redirect_uri: str = current_app.config['ONE_LOGIN_AUTH_REDIRECT_URI']
+        self.identity_redirect_uri: str = current_app.config['ONE_LOGIN_IDENTITY_REDIRECT_URI']
+        self.did_url: str = current_app.config['ONE_LOGIN_DID_URL']
+        self.post_logout_redirect_uri = current_app.config['ONE_LOGIN_POST_LOGOUT_REDIRECT_URI']
         self.scope: str = "openid phone email"
         self.claims: Dict[str, Dict[str, None]] = self.build_claims()
         self.public_key: bytes = self.load_public_key()
@@ -17,6 +20,7 @@ class OneLoginConfig:
         self.authorization_endpoint: str = self.metadata['authorization_endpoint']
         self.user_info_endpoint: str = self.metadata['userinfo_endpoint']
         self.token_endpoint: str = self.metadata['token_endpoint']
+        self.end_session_endpoint = self.metadata['end_session_endpoint']
         self.registration_endpoint: str = self.metadata['registration_endpoint']
         self.jwks_uri: str = self.metadata['jwks_uri']
 
@@ -41,6 +45,12 @@ class OneLoginConfig:
             "userinfo": {
                 "https://vocab.account.gov.uk/v1/coreIdentityJWT": None,
                 "https://vocab.account.gov.uk/v1/address": None,
-                "https://vocab.account.gov.uk/v1/returnCode": None
+                "https://vocab.account.gov.uk/v1/returnCode": None,
+                "https://vocab.account.gov.uk/v1/passport": None,
+                "https://vocab.account.gov.uk/v1/drivingPermit": None
             }
         }
+
+@lru_cache(maxsize=1)
+def get_onelogin_config():
+    return OneLoginConfig()
--- grc/one_login/one_login_user_info_request.py
+++ grc/one_login/one_login_user_info_request.py
@@ -1,7 +1,9 @@
-from grc.one_login import OneLoginTokenStorage
+from flask import session
 from grc.one_login.one_login_config import OneLoginConfig
+from grc.one_login.one_login_jwt_handler import JWTHandler
 from grc.utils.logger import LogLevel, Logger
 import requests
+from typing import Tuple, List
 
 logger = Logger()
 
@@ -10,10 +12,44 @@ class OneLoginUserInfoRequest:
     def __init__(self, config: OneLoginConfig):
         self.config = config
 
-    def fetch_user_info(self):
+    def create_auth_session(self, access_token: str, id_token: str):
+        user_info = self._fetch_user_info(access_token)
+        session["user"] = {
+            "sub": user_info.get("sub"),
+            "email": user_info.get("email"),
+            "phone_number": user_info.get("phone_number"),
+            "identity_verified": False,
+            "id_token": id_token
+        }
+
+    def update_auth_session_with_identity(self, access_token: str, id_token:str):
+        user_info = self._fetch_user_info(access_token)
+
+        if "user" not in session:
+            session["user"] = {
+                "sub": user_info.get("sub"),
+                "email": user_info.get("email"),
+                "phone_number": user_info.get("phone_number"),
+                "id_token": id_token
+            }
+
+        context_jwt = user_info.get("https://vocab.account.gov.uk/v1/coreIdentityJWT")
+        name, dob = self._get_names_dob_from_context_jwt(context_jwt)
+
+        updates = {
+            "name": name,
+            "dob": dob,
+            "address": user_info.get("https://vocab.account.gov.uk/v1/address"),
+            "driving_permit": user_info.get("https://vocab.account.gov.uk/v1/drivingPermit"),
+            "passport": user_info.get("https://vocab.account.gov.uk/v1/passport"),
+            "identity_verified": True
+        }
+        session["user"].update(updates)
+        session.modified = True
+
+    def _fetch_user_info(self, access_token:str):
         try:
-            access_token = OneLoginTokenStorage.get_access_token()
-            headers = self._build_user_info_request_headers(access_token=access_token)
+            headers = OneLoginUserInfoRequest._build_user_info_request_headers(access_token=access_token)
             response = requests.get(url=self.config.user_info_endpoint, headers=headers)
 
             if response.status_code != 200:
@@ -22,13 +58,40 @@ class OneLoginUserInfoRequest:
                 raise Exception(error_message)
 
             user_info = response.json()
-            logger.log(LogLevel.DEBUG, str(user_info))
             return user_info
 
         except Exception as e:
             error_message = f"Failed to fetch user information due to: {str(e)}"
             logger.log(LogLevel.ERROR, error_message)
             raise Exception(error_message)
+
+    def _get_names_dob_from_context_jwt(self, context_jwt_token:str) -> Tuple[str, str]:
+        public_key = JWTHandler.get_public_key_from_did(did_url=self.config.did_url, jwt_token=context_jwt_token)
+        decoded_context_token = JWTHandler.decode_jwt_with_key(jwt_token=context_jwt_token, public_key=public_key, algorithm="ES256")
+        credentialSubject = decoded_context_token.get('vc', {}).get('credentialSubject')
+
+        if credentialSubject:
+            names = credentialSubject.get('name')
+            name = OneLoginUserInfoRequest._extract_current_name(names)
+            dob_list = credentialSubject.get("birthDate", [])
+            dob = dob_list[0]["value"] if dob_list else None
+            return name, dob
+
+        raise Exception('Could not get credentials from context JWT.')
+
+    @staticmethod
+    def _extract_current_name(names) -> str:
+        for name_entry in names:
+            if name_entry.get('validUntil') is None:
+                name_parts = name_entry.get('nameParts', [])
+
+                given_names = [part['value'] for part in name_parts if part['type'] == 'GivenName']
+                family_name = next((part['value'] for part in name_parts if part['type'] == 'FamilyName'), "")
+
+                full_name = " ".join(given_names) + " " + family_name
+                return full_name.strip()
+        raise Exception('No valid name present')
+
 
     @staticmethod
     def _build_user_info_request_headers(access_token:str):
