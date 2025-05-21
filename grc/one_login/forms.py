11f4baa53bf12a42b77be23d7483da7aa059e12e
sgmselli <sellings.matthew@gmail.com>
2025-05-21 11:08:28 +0100

    reference number path

--- grc/one_login/forms.py
+++ grc/one_login/forms.py
@@ -2,18 +2,25 @@ from flask_wtf import FlaskForm
 from wtforms import RadioField, StringField
 from wtforms.validators import InputRequired
 from grc.utils.form_custom_validators import StrictRequiredIf, validate_reference_number
+from grc.lazy.lazy_fields import LazyRadioField
+from grc.business_logic.constants.start_application import StartApplicationConstants as c
+from grc.lazy.lazy_form_custom_validators import LazyDataRequired
+
 
 class ReferenceCheckForm(FlaskForm):
-    has_reference = RadioField(
-        'Do you have a reference number?',
-        choices=[('yes', 'Yes'), ('no', 'No')],
-        validators=[InputRequired()]
+    has_reference = LazyRadioField(
+        lazy_choices=[
+            ('FIRST_VISIT', c.NO),
+            ('HAS_REFERENCE', c.YES_WITH_REFERENCE_NUMBER),
+            ('LOST_REFERENCE', c.YES_LOST_REFERENCE_NUMBER)
+        ],
+        validators=[LazyDataRequired(lazy_message=c.IS_FIRST_VISIT_ERROR)]
     )
     reference = StringField(
         validators=[
             StrictRequiredIf(
                 'has_reference',
-                'yes',
+                'HAS_REFERENCE',
                 message='Please enter your reference number',
                 validators=[validate_reference_number]
             )
