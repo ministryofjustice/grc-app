98bae92d58890f50764f7e2fbbca1217d1fd611c
Matthew Sellings <99349293+sgmselli@users.noreply.github.com>
2025-05-27 14:42:09 +0100

    Staging Glimr Integration work (#323)
    * Glimr changes
    
    * Include glimr api key and glimr base url into admin deployment yaml (#319)
    
    * RST-7291 Loader on case register (#320)
    
    * Add loading spinner when case register button is clicked
    
    * Disable api calls to GLiMR when loading
    
    * Remove debug logs
    
    * Change 'phone' to 'contactPhone' in GlimrNewCase params.
    
    * RST 7292 Error Handling (#321)
    
    * Add error handling, Handle None type error on our side
    
    * Solve accessibility failures
    
    * Solve accessibility failures
    
    * Solve accessibility failures
    
    * Fix errors problem
    
    * Fix errors problem
    
    * Include salutation and middle name in first names for glimr
    
    * Solve error: remove self from staticmethod
    
    * Add overseas online mapping code (#322)
    
    * Edit params to include online mapping code and remove unused parameters in api
    
    * Refactor GLiMR data class and update glimr mock api to accept Overseas mapping code

---
+++ tests/end_to_end_tests/journey_admin/login_and_confirmation/section.py
@@ -0,0 +1,70 @@
+from playwright.async_api import Page
+from tests.end_to_end_tests.helpers.e2e_assert_helpers import AssertHelpers
+from tests.end_to_end_tests.helpers.e2e_page_helpers import PageHelpers
+import tests.end_to_end_tests.journey_admin.data as data
+
+
+
+async def run_checks_on_section(page: Page, asserts: AssertHelpers, helpers: PageHelpers):
+
+    # ------------------------------------------------
+    # ---- Login page
+    # ------------------------------------------------
+    await asserts.url('/')
+    await asserts.accessibility()
+    assert await page.inner_text('a.govuk-header__link.govuk-header__service-name') == 'Download GRC applications'
+    await asserts.h1('Login to download GRC applications')
+    await asserts.number_of_errors(0)
+
+    # Don't enter an Email Address or password, click Continue button, see an error message
+    await helpers.fill_textbox(field='email_address', value='')
+    await helpers.fill_textbox(field='password', value='')
+    await helpers.click_button('Login')
+    await asserts.url('/')
+    await asserts.accessibility(page_description='No email address or password entered')
+    await asserts.h1('Login to download GRC applications')
+    await asserts.number_of_errors(2)
+    await asserts.error(field='email_address', message='Enter your email address')
+    await asserts.error(field='password', message='Enter your password')
+
+    # Enter a valid Email Address, click Continue button, see the Security Code page
+    await helpers.fill_textbox(field='email_address', value=data.EMAIL_ADDRESS)
+    await helpers.fill_textbox(field='password', value=data.PASSWORD)
+    await helpers.click_button('Login')
+
+    # ------------------------------------------------
+    # ---- Security Code page
+    # ------------------------------------------------
+    await asserts.url('/sign-in-with-security_code')
+    await asserts.accessibility()
+    await asserts.h1('Check your email')
+    await asserts.number_of_errors(0)
+
+# Don't enter a Security Code, click Continue button, see an error message
+    await helpers.fill_textbox(field='security_code', value='')
+    await helpers.click_button('Continue')
+    await asserts.url('/sign-in-with-security_code')
+    await asserts.accessibility(page_description='No security code entered')
+    await asserts.h1('Check your email')
+    await asserts.number_of_errors(1)
+    await asserts.error(field='security_code', message='Enter a security code')
+
+    # Enter an invalid Security Code, click Continue button, see an error message
+    await helpers.fill_textbox(field='security_code', value='4444')  # Note: Don't use a 5-digit code, otherwise this test will break once every 10,000 runs!
+    await helpers.click_button('Continue')
+    await asserts.url('/sign-in-with-security_code')
+    await asserts.accessibility(page_description='Invalid security code entered')
+    await asserts.h1('Check your email')
+    await asserts.number_of_errors(1)
+    await asserts.error(field='security_code', message='Enter the security code that we emailed you')
+
+    # Enter a valid Security Code, click Continue button
+    await helpers.fill_textbox(field='security_code', value='11111')
+    await helpers.click_button('Continue')
+
+    # ------------------------------------------------
+    # ---- Is First Visit page
+    # ------------------------------------------------
+    await asserts.url('/applications')
+    await asserts.accessibility()
+    await asserts.h1('View and download GRC applications')
