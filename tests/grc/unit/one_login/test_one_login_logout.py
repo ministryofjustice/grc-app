c7c28e4ba539e47b690e0797aa003133d407ea3c
Timi Olubusi <timiolubusi@icloud.com>
2025-06-19 12:23:47 +0100

    Removing unused import

--- tests/grc/unit/one_login/test_one_login_logout.py
+++ tests/grc/unit/one_login/test_one_login_logout.py
@@ -1,6 +1,5 @@
 import pytest
 from unittest.mock import patch, MagicMock
-from datetime import date
 
 def test_logout_redirect_url_to_confirmation_page(logout, app):
     with app.test_request_context():
